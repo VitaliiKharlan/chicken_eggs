deef9245f0e7b55b8c3cb818ed5476144b9deb35
vitaliy.kharlan.1977@gmail.com <vitaliy.kharlan.1977.gmail.com>
2025-10-07 18:59:29 +0300

    start LoadingScreen

---
+++ lib/core/router/router.gr.dart
@@ -0,0 +1,155 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+part of 'router.dart';
+
+/// generated route for
+/// [GameScreen]
+class GameRoute extends PageRouteInfo<void> {
+  const GameRoute({List<PageRouteInfo>? children})
+    : super(GameRoute.name, initialChildren: children);
+
+  static const String name = 'GameRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const GameScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [HowToPlayScreen]
+class HowToPlayRoute extends PageRouteInfo<void> {
+  const HowToPlayRoute({List<PageRouteInfo>? children})
+    : super(HowToPlayRoute.name, initialChildren: children);
+
+  static const String name = 'HowToPlayRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const HowToPlayScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [LeaderScreen]
+class LeaderRoute extends PageRouteInfo<void> {
+  const LeaderRoute({List<PageRouteInfo>? children})
+    : super(LeaderRoute.name, initialChildren: children);
+
+  static const String name = 'LeaderRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const LeaderScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [LevelScreen]
+class LevelRoute extends PageRouteInfo<void> {
+  const LevelRoute({List<PageRouteInfo>? children})
+    : super(LevelRoute.name, initialChildren: children);
+
+  static const String name = 'LevelRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const LevelScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [LoadingScreen]
+class LoadingRoute extends PageRouteInfo<void> {
+  const LoadingRoute({List<PageRouteInfo>? children})
+    : super(LoadingRoute.name, initialChildren: children);
+
+  static const String name = 'LoadingRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const LoadingScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [MenuScreen]
+class MenuRoute extends PageRouteInfo<void> {
+  const MenuRoute({List<PageRouteInfo>? children})
+    : super(MenuRoute.name, initialChildren: children);
+
+  static const String name = 'MenuRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const MenuScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [ProfileScreen]
+class ProfileRoute extends PageRouteInfo<void> {
+  const ProfileRoute({List<PageRouteInfo>? children})
+    : super(ProfileRoute.name, initialChildren: children);
+
+  static const String name = 'ProfileRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const ProfileScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [SettingsScreen]
+class SettingsRoute extends PageRouteInfo<void> {
+  const SettingsRoute({List<PageRouteInfo>? children})
+    : super(SettingsRoute.name, initialChildren: children);
+
+  static const String name = 'SettingsRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const SettingsScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [ShopScreen]
+class ShopRoute extends PageRouteInfo<void> {
+  const ShopRoute({List<PageRouteInfo>? children})
+    : super(ShopRoute.name, initialChildren: children);
+
+  static const String name = 'ShopRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const ShopScreen();
+    },
+  );
+}
